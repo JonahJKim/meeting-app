4995f465340fa03f8e16b388162bae578b6ed61c
Jonah Kim <jonahkim03@gmail.com>
2022-05-01 01:13:14 -0700

    Before CSS and Nav Bar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import logo from './logo.svg';
+import AllMeetupsPage from './pages/AllMeetups.js';
+import NewMeetupPage from './pages/NewMeetup.js';
+import FavoritesPage from './pages/Favorites.js';
+import MainNavigation from './components/layout/MainNavigation'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom'
 
 function App() {
+  // localhost: 3000
+  // something like my-page.com/{path} i.e. /favorites
   return (
-    <div></div>
+    <Router>
+      <div>
+        <MainNavigation />
+        <Routes>
+            <Route path='/' element={<AllMeetupsPage />}/> 
+            <Route path='/new-meetup' element={<NewMeetupPage />}/> 
+            <Route path='/favorites' element={<FavoritesPage />}/> 
+          </Routes>
+      </div>
+    </Router>
   );
 }
 
