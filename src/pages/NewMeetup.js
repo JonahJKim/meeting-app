cada5671e034d6f43926e9b5cabe41cbe38aa76f
Jonah Kim <jonahkim03@gmail.com>
2022-05-02 13:44:21 -0700

    Added post to firebase database

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,24 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetupPage() {
-    return <div>New Meetup Page</div>
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-getting-started-7f5fe-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),      // typically in JSON format (most popular data form for transferring data)
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ); // allows us to send HTTP requests
+    }
+
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandler}/>
+
+    </section>
 }
 
 export default NewMeetupPage;
